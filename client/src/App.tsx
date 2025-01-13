722c60d79a05cafdfde86e928f7e6d6d6633c926
Victor Toschi <victor.toschi@hotmail.com>
2025-01-13 10:45:41 -0300

    Update html structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,11 @@
-function App() {
+import CreditCardModule from './modules/CreditCardModule/CreditCardModule';
+
+const App = () => {
 	return (
-		<div className="min-h-screen bg-gray-100 flex items-center justify-center">
-			<div className="bg-white p-8 rounded-lg shadow-md">
-				<h1 className="text-2xl font-bold text-gray-800 mb-4">
-					Credit Card Validator
-				</h1>
-			</div>
-		</div>
+		<section className="flex flex-col items-center justify-center h-screen">
+			<CreditCardModule />
+		</section>
 	);
-}
+};
 
 export default App;
