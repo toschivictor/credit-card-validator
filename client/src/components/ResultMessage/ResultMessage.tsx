6d72e1be369d860b94ff3c8c0eacb7af956637b9
Victor Toschi <victor.toschi@hotmail.com>
2025-01-13 10:18:17 -0300

    Update message color

--- client/src/components/ResultMessage/ResultMessage.tsx
+++ client/src/components/ResultMessage/ResultMessage.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 interface ResultMessageProps {
 	message: string | null;
+	isValid?: boolean;
 }
 
-const ResultMessage = ({ message }: ResultMessageProps) => {
-	return message ? <p className="text-lg">{message}</p> : null;
+const ResultMessage = ({ message, isValid }: ResultMessageProps) => {
+	return message ? (
+		<p className={`text-lg ${isValid ? 'text-green-500' : 'text-red-500'}`}>
+			{message}
+		</p>
+	) : null;
 };
 
 export default ResultMessage;
