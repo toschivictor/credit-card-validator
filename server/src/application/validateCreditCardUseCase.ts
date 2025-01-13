222b5c7ee7cf7b291d97e301905960eba643b052
Victor Toschi <victor.toschi@hotmail.com>
2025-01-13 09:14:07 -0300

    Add string validation

--- server/src/application/validateCreditCardUseCase.ts
+++ server/src/application/validateCreditCardUseCase.ts
@@ -1,6 +1,9 @@
 import { validateCreditCard } from '../domain/validateCreditCard';
 
 export const validateCreditCardUseCase = (cardNumber: string): boolean => {
+	if (typeof cardNumber !== 'string') {
+		throw new Error('Card number must be a string');
+	}
 	if (!cardNumber) {
 		throw new Error('Card number is required');
 	}
